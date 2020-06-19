6875c9ee1bea877048095e5c657b870de4dd0079
Leandro C. Ramos <leandrosc.ramos@gmail.com>
2020-06-19 19:31:11 -0300

    MacroProcessos

--- php/View/ViewMacroProcess.php
+++ php/View/ViewMacroProcess.php
@@ -82,7 +82,7 @@ include_once $_SESSION["root"].'php/Util/Util.php';
 							</div>
 							
 						</div>
-						<?php Util::debug($procTypes); ?>
+						<?php //Util::debug($procTypes); ?>
 						<!-- Modal - Cadastro de Tipos de Macroprocessos -->
 						<div id="modalCreate" class="modal fade">
 							<div class="modal-dialog">
